456f97a7c2217d7a323e107f7c35bed6127a49af
Andrey Yurov <a.yurov6@gmail.com>
2023-02-19 21:22:39 +0700

    Embeded map to the contact page

--- contact.php
+++ contact.php
@@ -40,6 +40,8 @@
   <main class="text-center">
     Contact Form Goes Here
   </main>
+  <main class="text-center">    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.1556388386243!2d-122.4763189!3d37.809823300000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808586ec8cf8899d%3A0x531cddc68c112b26!2s1000%20Marine%20Dr%2C%20San%20Francisco%2C%20CA%2094129%2C%20USA!5e0!3m2!1sen!2sth!4v1676816022005!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </main>
 
   <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
